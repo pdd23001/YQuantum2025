c135d8248f2b03bc6759edc991043a89a43bfc67
Parth Danve <parth.danve@uconn.edu>
2025-04-12 23:12:59 -0400

    wtv

--- P1.py
+++ P1.py
@@ -8,4 +8,3 @@ qc = QuantumCircuit.from_qasm_file('P1_little_peak.qasm')
 
 bq = bluequbit.init("tbtC35WEFeL9IXYkJoeUgFIoQzYu8lQQ")
 result = bq.run(qc, device='cpu') 
-print(result.get_statevector())
